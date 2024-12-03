cf800c860e31230e991cbd787f5ca8b1107a3138
Gustavo Vilela Mitraud <gvmitraud@protonmail.com>
2024-12-03 10:04:22 -0300

    Added terraform provider

---
+++ provisioning/terraform/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    huaweicloud = {
+      source  = "huaweicloud/huaweicloud"
+      version = ">= 1.36.0"
+    }
+  }
+}
+
+variable "secret_key" {}
+variable "access_key" {}
+
+provider "huaweicloud" {
+    region = "sa-brazil-1"
+    secret_key = var.secret_key
+    access_key = var.access_key
+}
